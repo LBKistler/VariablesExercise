84a4244996c16d6109d27bd99b812184c6e499ee
“lbkistler” <loribethkistler@gmail.com>
2022-03-02 01:02:22 -0600

    uploaded VariablesExercise

---
+++ variablesExercise.js
@@ -0,0 +1,55 @@
+// 1
+// CONST or Constant
+
+// 2
+let num = 20;
+console.log(num);
+
+// 3
+num = 15;
+console.log(num);
+
+// 4
+num = num + 7;
+console.log(num);
+
+// 5
+num--;
+console.log(num);
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+console.log(blank);
+
+// BONUS
+// B1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+console.log(quarter);
+console.log(dime);
+console.log(nickel);
+console.log(penny);
+
+// B2
+console.log(quarter, dime, nickel, penny);
+
+// B3
+let letters = "qwertyuiop";
+console.log(letters);
+
+// B4
+console.log("qwertyuiop".length);
+console.log("qwertyuiop"[9]);
+
+
+
+
